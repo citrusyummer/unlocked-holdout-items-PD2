e92b12c9755ade0347879e171ca730039d06ef0d
citrus <citrusyummer@billgates.sex>
2026-05-12 22:51:58 +0100

    Add files via upload

---
+++ Unlocked Holdout Items/unlocker.lua
@@ -0,0 +1,8 @@
+-- skirmish is the internal name for Holdout btw
+function GenericDLCManager:is_content_skirmish_locked(category, entry)
+	return false
+end
+
+function GenericDLCManager:skirmish_locked_content()
+	return {}
+end
